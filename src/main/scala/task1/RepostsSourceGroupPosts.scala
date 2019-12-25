ac5658143f5b603a78fc8c2699b8618f06671113
omegacat <anisimov.develop@gmail.com>
2019-12-25 21:50:39 +0300

    Edit third exercise;

--- src/main/scala/task1/RepostsSourceGroupPosts.scala
+++ src/main/scala/task1/RepostsSourceGroupPosts.scala
@@ -1,6 +1,7 @@
 package task1
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions.{collect_list, explode, size}
 
 /*
 получить репосты исходных постов группы itmo (posts.json) из постов пользователей
@@ -8,20 +9,28 @@ import org.apache.spark.sql.SparkSession
  */
 object RepostsSourceGroupPosts {
   def main(args: Array[String]): Unit = {
-    val sparkSession = SparkSession.builder().appName("task1_Top20Posts").master("local[*]").getOrCreate()
-    val df_group_posts = sparkSession.read.json("data/posts_api.json").repartition(10)
-    val df_follower_posts = sparkSession.read.json("data/followers_posts_api_final.json").repartition(10)
+    val sparkSession = SparkSession.builder().appName("task1_RepostsSourceGroupPosts").master("local[*]").getOrCreate()
     import sparkSession.implicits._
 
-    val listIds = df_group_posts.filter($"id".isNotNull && $"reposts.count".gt(0))
-      .select("id")
-      .map(id => id.getLong(0))
-      .collectAsList()
+//    собираем посты подписчиков, которые являются репостами
+    val followers_posts = sparkSession.read.json("data/followers_posts_api_final.json")
+      .select("from_id", "copy_history")
+      .filter(row => row.get(1) != null)
+      .withColumn("copy_history", explode($"copy_history"))
 
-    df_follower_posts.filter($"copy_history".isNotNull)
-      .select("copy_history.from_id")
-      .map(id => id.getLong(0))
-      .filter(from_id => listIds.contains(from_id))
-      .show()
+//    берём посты, для которых copy_history.owner_id = -94 - т.е. репосты из группы ITMO
+    val posts = followers_posts.select("from_id", "copy_history.id", "copy_history.owner_id")
+      .filter("copy_history.owner_id == -94")
+      .toDF("from_id", "id", "owner")
+      .sort($"id")
+
+//    приводим данные к виду "group_post_id, Array (user_post_ids)"
+    posts.groupBy("id")
+      .agg(collect_list("from_id"))
+      .withColumnRenamed("id", "group_post_id")
+      .withColumnRenamed("collect_list(from_id)", "user_post_ids")
+      .orderBy(size($"user_post_ids").desc)
+      .limit(20)
+      .write.json("results/task1/reposts_source")
   }
 }
